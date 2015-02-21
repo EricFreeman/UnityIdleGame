4582819edbab68fc6f8c33e321ee6e102ac8b39b
Eric Freeman <eric.freeman@extendhealth.com>
2015-02-20 19:25:17 -0700

    I have a director that gives you basic money per second.

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Director : MonoBehaviour
+    {
+        public float MoneyPerSecond = 1;
+        public float CurrentMoney = 0;
+
+        public Text MoneyText;
+
+        void Update()
+        {
+            CurrentMoney += MoneyPerSecond;
+            MoneyText.text = CurrentMoney.ToString("C");
+        }
+    }
+}
